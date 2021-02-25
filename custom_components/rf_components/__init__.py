d8eb26cdeee43200c44062485642f8de5298b11a
mtorchalla <m.torchalla@gmail.com>
2021-02-25 22:37:01 +0100

    Add service descriptions

--- custom_components/rf_components/__init__.py
+++ custom_components/rf_components/__init__.py
@@ -15,14 +15,16 @@ COMPONENT_DOMAIN = 'rf_components'
 COMPONENT_SERVICES = 'rf_components-services'
 
 
-def add_new_switch_to_yaml(call):
-    """Handle the service call."""
-    name = call.data.get("name", "test")
-    _LOGGER.debug("RF_COMPONENTS: Got from service call: {}".format(name))
+
 
 
 def setup(hass, _config):
     """Set up a rf component."""
+
+    def add_new_switch_to_yaml(call):
+        """Handle the service call."""
+        name = call.data.get("name", "test")
+        _LOGGER.debug("RF_COMPONENTS: Got from service call: {}".format(name))
 
     hass.data[COMPONENT_SERVICES] = {}
     _LOGGER.debug('setup')
